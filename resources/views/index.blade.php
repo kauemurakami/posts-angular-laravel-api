51bfce509791169beed86649200db88f626ac014
kaue murakami <kauetmurakami@gmail.com>
2019-06-28 15:05:24 -0300

    coreeção de segurança csrf

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
     rel="stylesheet">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Laravel</title>
     <base href="/">
 
